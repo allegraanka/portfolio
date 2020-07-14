619ecb1156a632a8d9c201b8ae247e5529d2af92
Allegra Megan Anka <allegra.anka@gmail.com>
2020-07-13 22:46:17 -0400

    made some progress converting project cards to grid list tiles... not sure how i feel about it yet

--- src/components/project-data/project-data.component.jsx
+++ src/components/project-data/project-data.component.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import Tabletop from 'tabletop';
 
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
 
 import './project-data.styles.scss';
 
@@ -35,37 +32,23 @@ class ProjectData extends Component {
         const { data } = this.state;
 
         return (
-          <div className="project-data-container">
-            <h1 className="project-data-title">Recent Projects</h1>
-            <div id="project-list">
-                {
-                  data.map(obj => {
-                    return (
-                      <Card key={obj.title} className="root">
-                        <CardActionArea>
-                          <CardMedia title={`${obj.title}`} className="media"/>
-                            <p>image here</p>
-                          <CardContent>
-                          <Typography gutterBottom variant="h5" component="h2">
-                            {obj.title}
-                          </Typography>
-                          <Typography variant="body2" color="textSecondary" component="p">
-                            {obj.description}
-                          </Typography>
-                          </CardContent>
-                        </CardActionArea>
-                        <CardActions>
-                        <Button size="small" color="primary">
-                          <a href={obj.live_url}>Live</a>
-                        </Button>
-                        <Button size="small" color="primary">
-                          <a href={obj.github_url}>GitHub</a>
-                        </Button>
-                      </CardActions>
-                      </Card>
-                    )
-                  })
-                }
+          <div className='project-page-content-container'>
+            <div className='project-description-container'>This is something about these cool projects I've made.</div>
+            <div className="project-data-container">
+              <GridList cellHeight={180} cols={2} spacing={6} className="gridList">
+              <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+                <ListSubheader component="div">Recent Projects</ListSubheader>
+              </GridListTile>
+              {data.map(obj => (
+                <GridListTile key={obj.title} className='tile'>
+                <img src={obj.image_url} alt={obj.title} className="tile-image"/>
+                <GridListTileBar
+                  title={obj.title}
+                  subtitle={<span>{obj.description}</span>}
+                />
+                </GridListTile>
+              ))}
+              </GridList>
             </div>
           </div>
         );
